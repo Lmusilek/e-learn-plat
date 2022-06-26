34d131a6319127ef86923d4a72d07005526d1d0a
Lmusilek <lukas.musilek@gmail.com>
2022-06-26 15:40:29 +0000

    added online status to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
   
   include PublicActivity::StoreController
   
+  after_action :user_activity
+  
   before_action :set_global_variables, if: :user_signed_in?
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
@@ -16,6 +18,10 @@ class ApplicationController < ActionController::Base
   
   
   private
+
+  def user_activity
+    current_user.try :touch
+  end
 
   def user_not_authorized #pundit
     flash[:alert] = "You are not authorized to perform this action."
